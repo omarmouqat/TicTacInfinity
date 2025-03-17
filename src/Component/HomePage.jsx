272a2708056a1ce0558c3e863077ae3867b68795
Omar Mouqat <omar.dev.23.06.2003@gmail.com>
2025-03-17 10:04:17 +0000

    commit

--- src/Component/HomePage.jsx
+++ src/Component/HomePage.jsx
@@ -24,9 +24,9 @@ const HomePage = ({setMode, getMode, getisGameStarted, setIsGameStarted}) => {
         <h1 className='mx-10'>Welcome To Tic Tac Toe</h1>
       </div>
       <div className="mx-auto w-8/10 h-40/100 flex-4 flex flex-row justify-evenly items-center gap-[3dvw]">
-        <button data-tooltip-id='tooltip' data-tooltip-content="Play With Computer." onClick={()=>{setMode(1);}} className={`transition-colors duration-300 flex-1 h-1/2 w-1/2 stroke-[0.4] ${(getMode()===1)?"text-green-500":"text-black"}`}><Cpu className="flex-1 h-full w-full stroke-[0.4]"/></button>
-        <button data-tooltip-id='tooltip' data-tooltip-content="Play With Friends." onClick={()=>{setMode(2);}} className={`transition-colors duration-300 flex-1 h-1/2 w-1/2 stroke-[0.4] ${(getMode()===2)?"text-green-500":"text-black"}`}><Network className="flex-1 h-full w-full stroke-[0.4]"/></button>
-        <button data-tooltip-id='tooltip' data-tooltip-content="Play infinitely with friends." onClick={()=>{setMode(3);}} className={`transition-colors duration-300 flex-1 h-1/2 w-1/2 stroke-[0.4] ${(getMode()===3)?"text-green-500":"text-black"}`}><Infinity className="flex-1 h-full w-full stroke-[0.4]"/></button>
+        <button data-tooltip-id='tooltip' data-tooltip-content="Ai Mode" onClick={()=>{setMode(1);}} className={`transition-colors duration-300 flex-1 h-1/2 w-1/2 stroke-[0.4] ${(getMode()===1)?"text-green-500":"text-black"}`}><Cpu className="flex-1 h-full w-full stroke-[0.4]"/></button>
+        <button data-tooltip-id='tooltip' data-tooltip-content="Friends Mode" onClick={()=>{setMode(2);}} className={`transition-colors duration-300 flex-1 h-1/2 w-1/2 stroke-[0.4] ${(getMode()===2)?"text-green-500":"text-black"}`}><Network className="flex-1 h-full w-full stroke-[0.4]"/></button>
+        <button data-tooltip-id='tooltip' data-tooltip-content="Infinit Mode" onClick={()=>{setMode(3);}} className={`transition-colors duration-300 flex-1 h-1/2 w-1/2 stroke-[0.4] ${(getMode()===3)?"text-green-500":"text-black"}`}><Infinity className="flex-1 h-full w-full stroke-[0.4]"/></button>
       </div>
       <div className="w-full h-20/100 flex-2 flex flex-col items-center" >
           <h2 className="text-xl">{(getMode()==1)?"Play With Computer.":(getMode()==2)?"Play With Friends.":(getMode()==3)?"Play infinitely with friends.":"Choose A Mode To Start."}</h2>
